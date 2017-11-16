a7d8fb8c62368ffc1ce3bcef55053e2698470d17
LordSquishers <patrick_asl@10.31.17.204>
2017-11-16 16:55:46 +0000

    Spruced up the hello world bit a little, made it more responsive.

--- Hello World/src/main/HelloWorld.java
+++ Hello World/src/main/HelloWorld.java
@@ -1,11 +1,17 @@
 package main;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
 		// This is a comment. Java comments are preceded by "//". Remove the
 		// "//" in the line bellow to uncomment it.
 
-		// System.out.prinln("Hello World");
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Whats your name? > ");
+		String name = sc.next();
+		System.out.println("Hello, " + name + ".");
+		sc.close();
 
 		// Run the program by pressing the green play button in the tool bar.
 	}
